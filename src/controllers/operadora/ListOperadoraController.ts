225856af928f0fcc4fd243969ff75e45b108dcfd
Leo Lourenço <leo.lsrd@gmail.com>
2026-07-20 23:52:06 -0300

    refactor: Revendo as validações do ListOperadoraService, ListOperadoraController e criado endpoint para listar todas operadoras.

--- src/controllers/operadora/ListOperadoraController.ts
+++ src/controllers/operadora/ListOperadoraController.ts
@@ -8,13 +8,30 @@ class ListOperadoraController {
 
       const listOperadoraService = new ListOperadoraService();
 
-      const operadoras = await listOperadoraService.listOperadoraStatus(
-        req,
-        res,
-        status,
-      );
+      const operadoras = await listOperadoraService.listOperadoraStatus(status);
 
-      return res.status(200).json(operadoras);
+      return res.status(200).json({
+        message: "Operadoras listadas com sucesso",
+        operadoras,
+      });
+    } catch (error) {
+      if (error instanceof Error)
+        return res.status(400).json({ error: error.message });
+
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
+
+  async listOperadoraAll(req: Request, res: Response) {
+    try {
+      const listOperadoraService = new ListOperadoraService();
+
+      const operadoras = await listOperadoraService.listOperadoraAll();
+
+      return res.status(200).json({
+        message: "Operadoras listadas com sucesso",
+        operadoras,
+      });
     } catch (error) {
       if (error instanceof Error)
         return res.status(400).json({ error: error.message });

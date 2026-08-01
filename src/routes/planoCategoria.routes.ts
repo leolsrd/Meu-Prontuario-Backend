4af71f989d62520bda38b6c0d38424837cacd430
Leo Lourenço <leo.lsrd@gmail.com>
2026-08-01 00:08:32 -0300

    feat: Criado o arquivo planoCategoria.routes para agrupar os endpoints dos plano categorias.

---
+++ src/routes/planoCategoria.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
+import { validateSchema } from "../middlewares/validateSchema";
+import { CreatePlanoCategoriaController } from "../controllers/planoCategoria/CreatePlanoCategoriaController";
+import {
+  createPlanoCategoriaSchema,
+  updatePlanoCategoriaSchema,
+} from "../schemas/planoCategoriaSchema";
+import { UpdatePlanoCategoriaController } from "../controllers/planoCategoria/UpdatePlanoCategoriaController";
+import { ListPlanoCategoriaController } from "../controllers/planoCategoria/ListPlanoCategoriaController";
+
+const planoCategoriaRoutes: Router = Router();
+
+// ? Rota para cadastrar plano categoria
+planoCategoriaRoutes.post(
+  "/",
+  isAuthenticated,
+  validateSchema(createPlanoCategoriaSchema),
+  new CreatePlanoCategoriaController().handle,
+);
+
+// ? Rota para alterar um plano categoria
+planoCategoriaRoutes.put(
+  "/plano-categoria/atualizar",
+  isAuthenticated,
+  validateSchema(updatePlanoCategoriaSchema),
+  new UpdatePlanoCategoriaController().handle,
+);
+
+// ? Rota para listar os planos categorias cadastrados
+planoCategoriaRoutes.get(
+  "/plano-categoria",
+  isAuthenticated,
+  new ListPlanoCategoriaController().handle,
+);
+
+export default planoCategoriaRoutes;

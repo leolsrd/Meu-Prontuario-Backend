551eec7f4082013a3c454921731dcc352a7e3d47
Leo Lourenço <leo.lsrd@gmail.com>
2026-08-02 01:56:15 -0300

    refactor: Removido trycatch, req, res, utils e add trim() no nome

--- src/services/planoCategoria/UpdatePlanoCategoriaService.ts
+++ src/services/planoCategoria/UpdatePlanoCategoriaService.ts
@@ -1,62 +1,50 @@
-import { Request, Response } from "express";
 import prismaClient from "../../prisma";
 import { PlanoCategoriaServiceProps } from "../../@types/planoCategoria.types";
-import { returnError } from "../../utils/returnError";
 
 class UpdatePlanoCategoriaService {
-  async execute(req: Request, res: Response, data: PlanoCategoriaServiceProps) {
-    try {
-      const planoCategoriaExists = await prismaClient.planoCategoria.findFirst({
-        where: {
-          idPlanoCategoria: data.idPlanoCategoria,
-        },
-      });
+  async execute(data: PlanoCategoriaServiceProps) {
+    const nome = data.nome?.trim();
 
-      if (!planoCategoriaExists) {
-        returnError({
-          messageConsole: "Plano Categoria não cadastrada",
-          statusCode: 400,
-          messageApi: "Plano Categoria não cadastrada",
-          res,
-        });
-        return;
-      }
-
-      const planoCategoriaWithSameName =
-        await prismaClient.planoCategoria.findFirst({
-          where: {
-            nome: data.nome,
-            idOperadora: data.idOperadora,
-            idPlanoCategoria: {
-              not: data.idPlanoCategoria,
-            },
-          },
-        });
-
-      if (planoCategoriaWithSameName) {
-        returnError({
-          messageConsole: "Plano Categoria já cadastrada para essa operadora",
-          statusCode: 400,
-          messageApi: "Plano Categoria já cadastrada para essa operadora",
-          res,
-        });
-        return;
-      }
-
-      const planoCategoria = await prismaClient.planoCategoria.update({
-        where: {
-          idPlanoCategoria: data.idPlanoCategoria,
-        },
-        data: {
-          ...data,
-        },
-      });
-
-      return planoCategoria;
-    } catch (error) {
-      if (error instanceof Error)
-        return res.status(400).json({ error: error.message });
+    if (!nome) {
+      throw new Error("O nome da plano categoria é obrigatório");
     }
+
+    const planoCategoriaExists = await prismaClient.planoCategoria.findFirst({
+      where: {
+        idPlanoCategoria: data.idPlanoCategoria,
+      },
+    });
+
+    if (!planoCategoriaExists) {
+      throw new Error("Plano Categoria não encontrada");
+    }
+
+    const planoCategoriaWithSameName =
+      await prismaClient.planoCategoria.findFirst({
+        where: {
+          nome: data.nome,
+          idOperadora: data.idOperadora,
+          idPlanoCategoria: {
+            not: data.idPlanoCategoria,
+          },
+        },
+      });
+
+    if (planoCategoriaWithSameName) {
+      throw new Error("Plano Categoria já cadastrada para essa operadora");
+    }
+
+    const planoCategoria = await prismaClient.planoCategoria.update({
+      where: {
+        idPlanoCategoria: data.idPlanoCategoria,
+        idOperadora: data.idOperadora,
+      },
+      data: {
+        ...data,
+      },
+    });
+
+    return planoCategoria;
   }
 }
 

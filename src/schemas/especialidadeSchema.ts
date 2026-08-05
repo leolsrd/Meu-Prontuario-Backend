83d7f6a9164bdcb9e83226986c36cf051170e906
Leo Lourenço <leo.lsrd@gmail.com>
2026-08-05 19:28:07 -0300

    refactor: updateEspecialidadeSchema recebe e valida params e body

--- src/schemas/especialidadeSchema.ts
+++ src/schemas/especialidadeSchema.ts
@@ -11,16 +11,23 @@ export const createEspecialidadeSchema = z.object({
   }),
 });
 
+const updateParamsSchemas = z.object({
+  id: z.uuid({ message: "O id da especialidade é obrigatório" }),
+});
+
+const updateBodySchemas = z.object({
+  nome: z
+    .string()
+    .min(5, "A especialidade deve ter pelo menos 5 caracteres")
+    .max(40, "A especialidade deve ter no máximo 40 caracteres")
+    .trim()
+    .optional(),
+  status: z.coerce
+    .boolean({ message: "O status deve ser true ou false" })
+    .optional(),
+});
+
 export const upadteEspecialidadeSchema = z.object({
-  body: z.object({
-    idEspecialidade: z.uuid({
-      message: "O id da especialidade é obrigatório",
-    }),
-    nome: z
-      .string()
-      .min(5, "A especialidade deve ter pelo menos 5 caracteres")
-      .max(40, "A especialidade deve ter no máximo 40 caracteres")
-      .trim(),
-    status: z.coerce.boolean({ message: "O status deve ser true ou false" }),
-  }),
+  params: updateParamsSchemas,
+  body: updateBodySchemas,
 });

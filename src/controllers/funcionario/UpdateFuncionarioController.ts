6f4f6a5fb2f4adcc35506c756e72fcfc717830d5
Leo Lourenço <leo.lsrd@gmail.com>
2026-08-30 16:06:29 -0300

    refactor: recebe idFuncionario via params

--- src/controllers/funcionario/UpdateFuncionarioController.ts
+++ src/controllers/funcionario/UpdateFuncionarioController.ts
@@ -4,7 +4,9 @@ import { UpdateFuncionarioService } from "../../services/funcionario/UpdateFunci
 class UpdateFuncionarioController {
   async handle(req: Request, res: Response) {
     try {
+      const idFuncionario = req.params.idFuncionario;
       const data = req.body;
+      data.idFuncionario = idFuncionario;
 
       const updateFuncionarioService = new UpdateFuncionarioService();
 

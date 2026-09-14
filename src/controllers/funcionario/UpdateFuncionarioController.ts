306fc9d20f9fd5528af9673ace3b865e5f7160c4
Leo Lourenço <leo.lsrd@gmail.com>
2026-09-14 00:15:19 -0300

    refactor(funcionario-controller): get idFuncionario params send to execute

--- src/controllers/funcionario/UpdateFuncionarioController.ts
+++ src/controllers/funcionario/UpdateFuncionarioController.ts
@@ -4,12 +4,15 @@ import { UpdateFuncionarioService } from "../../services/funcionario/UpdateFunci
 class UpdateFuncionarioController {
   async handle(req: Request, res: Response) {
     const idFuncionario = req.params.idFuncionario;
+
     const data = req.body;
-    data.idFuncionario = idFuncionario;
 
     const updateFuncionarioService = new UpdateFuncionarioService();
 
-    const funcionario = await updateFuncionarioService.execute(data);
+    const funcionario = await updateFuncionarioService.execute(
+      data,
+      idFuncionario as string,
+    );
 
     return res.status(200).json(funcionario);
   }
